dd316ba285da453cca4039cd4362238355081788
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2022-12-19 00:23:58 -0500

    Content language header

--- src/lib/node/get-language-from-request.ts
+++ src/lib/node/get-language-from-request.ts
@@ -2,10 +2,11 @@ import type { Request } from 'express';
 import { defaultLanguageCode, supportedLanguages, LanguageCode } from '@chirp/lib/utils/copy';
 
 export default function getLanguageFromRequest(request: Request): LanguageCode {
-  const languageHeader = request.get('Accept-Language') || '';
-  if ((supportedLanguages as string[]).includes(languageHeader)) {
-    return languageHeader as LanguageCode;
-  }
+  const languageHeader = (request.get('Accept-Language') || '').split(';')[0].split(',');
 
-  return defaultLanguageCode;
+  const match = languageHeader.find((languageCode) => {
+    return (supportedLanguages as string[]).includes(languageCode);
+  });
+
+  return (match as LanguageCode) || defaultLanguageCode;
 }
--- src/services/api/routes/index.ts
+++ src/services/api/routes/index.ts
@@ -2,6 +2,7 @@ import type { Application } from 'express';
 import { Router } from 'express';
 import type { MongoClient } from 'mongodb';
 import type { Logger } from 'pino';
+import getLanguageFromRequest from '@chirp/lib/node/get-language-from-request';
 import routeWrapper from '@chirp/lib/node/route-wrapper';
 import postChirper from './post-chirper';
 
@@ -18,6 +19,11 @@ export default function setupRoutes(
 ) {
   const router = Router();
   app.use('/v1', router);
+
+  app.use((request, response) => {
+    const language = getLanguageFromRequest(request);
+    response.set('Content-Language', language);
+  });
 
   const routeWrapperGenerator = (routeDefinition: Chirp.RequestDefinition) => 
     routeWrapper(router, routeDefinition, logger, mongoClient);
